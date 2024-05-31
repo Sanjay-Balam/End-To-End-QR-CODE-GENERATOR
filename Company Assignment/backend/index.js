773e41b48a40ef9cc44b8b19d8a04f4531e4384a
Sanjay Balam <b.sanjay0701@gmail.com>
2024-05-31 13:33:41 +0530

    first commit

---
+++ Company Assignment/backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const QRcode = require("qrcode");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/generate-url",(req,res)=>{
+    const { url } = req.body;
+
+    if (!url) {
+        return res.status(400).json({
+        message: "Bad Request: URL is required"
+        });
+    }
+
+    // convert url to dataurl 
+    QRcode.toDataURL(url,(err,qrcodeUrl)=>{
+        if(err){
+            res.status(500).json({
+                message:"Internal server error"
+            })
+        }
+        else{
+            res.json({
+                message:"QR link send successfully",
+                qrcodeUrl:qrcodeUrl
+            })
+        }
+    })
+
+})
+
+app.listen(3000);
